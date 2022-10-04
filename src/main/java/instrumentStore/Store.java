8a07147b197e2ca76cc52ac30bab71053aab8d42
Junjian Liu <junjianliu@Junjians-MacBook-Air.local>
2022-10-04 10:17:53 -0700

    Initial Status

---
+++ src/main/java/instrumentStore/Store.java
@@ -0,0 +1,12 @@
+public class Store {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+  
+    Scanner myScanner = new Scanner ();
+
+      System.out.println("Welcome to the instrument store! What instrument would you like to buy?");
+    }
+
+}
